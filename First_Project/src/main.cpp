078a469903008598bf6cc7a054d539eb18966999
yoavjavits <yoavjavits@campus.technion.ac.il>
2022-03-30 13:51:34 +0300

    Third Commit

--- First_Project/src/main.cpp
+++ First_Project/src/main.cpp
@@ -1,9 +1,44 @@
-#include <Arduino.h>
+/*
+  Blink with Function sketch
+  function-blink.ino
+  Use for PlatformIO demo
 
-void setup() {
-  // put your setup code here, to run once:
+  DroneBot Workshop 2021
+  https://dronebotworkshop.com
+*/
+
+#include <Arduino.h>
+// Define LED pin
+#define LED_PIN 13
+
+void blink_led(int LED, int delaytime)
+{
+  // Set output HIGH for specified time
+  digitalWrite(LED, HIGH);
+  delay(delaytime);
+
+  // Set output LOW for specified time
+  digitalWrite(LED, LOW);
+  delay(delaytime);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  // Initialize LED pin as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  // Blink the LED at half-second interval six times
+  for (int i = 0; i < 6; i++)
+  {
+    blink_led(LED_PIN, 500);
+  }
+
+  // Blink the LED at two-second interval three times
+  for (int i = 0; i < 3; i++)
+  {
+    blink_led(LED_PIN, 2000);
+  }
 }
