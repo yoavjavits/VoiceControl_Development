436ff5387f33e76156c02340134cdfe36210ede5
yoavjavits <yoavjavits@campus.technion.ac.il>
2022-05-03 06:09:44 +0300

    Lot of changes

---
+++ TwoModelOnESP/lib/neural_network_wake_word/src/modelWakeWord.h
@@ -0,0 +1,7 @@
+#ifndef __converted_model_h_wake_word__
+#define __converted_model_h_wake_word__
+
+extern unsigned char converted_model_tflite[];
+extern unsigned int converted_model_tflite_len_wake_word;
+
+#endif
