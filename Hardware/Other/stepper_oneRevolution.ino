9a97c7eed889ac2e48750a6f3f932aa87a6ae1b8
Gilad Shmerler <shmerler@campus.technion.ac.il>
2022-05-25 17:19:18 +0300

    move 6 steppers

--- Hardware/Other/stepper_oneRevolution.ino
+++ Hardware/Other/stepper_oneRevolution.ino
@@ -2,25 +2,51 @@
 
 const int X_ENABLE_PIN       = 38;
 const int Y_ENABLE_PIN       = A2;
+const int Z_ENABLE_PIN       = A8;
+const int E0_ENABLE_PIN       = 24;
+const int E1_ENABLE_PIN       = 30;
+
+
+const int O_STEP_PIN         = 45;
+const int O_DIR_PIN          = 47;
+const int O_ENABLE_PIN       = 32;
+
 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
 
-Stepper myStepper1(stepsPerRevolution, A0, A1);
-Stepper myStepper2(stepsPerRevolution, A6, A7);
+Stepper myStepperX(stepsPerRevolution, A0, A1); // X axis stepper
+Stepper myStepperY(stepsPerRevolution, A6, A7); // Y axis stepper
+Stepper myStepperZ(stepsPerRevolution, 46, 48); // Z axis stepper
+Stepper myStepperE0(stepsPerRevolution, 26, 28); // E0 axis stepper
+Stepper myStepperE1(stepsPerRevolution, 36, 34); // E1 axis stepper
+Stepper myStepperO(stepsPerRevolution, O_STEP_PIN, O_DIR_PIN); // O axis stepper
+
 
 void setup() {
   // set the speed at 60 rpm:
-  myStepper1.setSpeed(120);
-  myStepper2.setSpeed(120);
+  myStepperX.setSpeed(120);
+  myStepperY.setSpeed(120);
+  myStepperZ.setSpeed(120);
+  myStepperE0.setSpeed(120);
+  myStepperE1.setSpeed(120);
+  myStepperO.setSpeed(120);
 
   pinMode(X_ENABLE_PIN, OUTPUT);
   pinMode(Y_ENABLE_PIN, OUTPUT);
+  pinMode(Z_ENABLE_PIN, OUTPUT);
+  pinMode(E0_ENABLE_PIN, OUTPUT);
+  pinMode(E1_ENABLE_PIN, OUTPUT);
+  pinMode(O_ENABLE_PIN, OUTPUT);
 
-  digitalWrite(X_ENABLE_PIN, LOW);
+  digitalWrite(X_ENABLE_PIN, LOW); 
   digitalWrite(Y_ENABLE_PIN, LOW);
+  digitalWrite(Z_ENABLE_PIN, LOW);
+  digitalWrite(E0_ENABLE_PIN, LOW);
+  digitalWrite(E1_ENABLE_PIN, LOW);
+  digitalWrite(O_ENABLE_PIN, LOW);
   pinMode(LED_BUILTIN, OUTPUT);
-
-  // initialize the serial port:
+  
+  // initialize the serial port: 
   Serial.begin(9600);
   delay(2000);
 }
@@ -28,15 +54,24 @@ void setup() {
 void loop() {
   // step one revolution  in one direction:
   Serial.println("clockwise");
-  myStepper1.step(stepsPerRevolution);
-  myStepper2.step(stepsPerRevolution);
+  myStepperX.step(stepsPerRevolution);
+  myStepperY.step(stepsPerRevolution);
+  myStepperZ.step(stepsPerRevolution);
+  myStepperE0.step(stepsPerRevolution);
+  myStepperE1.step(stepsPerRevolution);
+  myStepperO.step(stepsPerRevolution);
   digitalWrite(LED_BUILTIN, LOW);
   delay(2000);
 
   // step one revolution in the other direction:
   Serial.println("counterclockwise");
-  myStepper1.step(-stepsPerRevolution);
-  myStepper2.step(-stepsPerRevolution);
+  myStepperX.step(-stepsPerRevolution);
+  myStepperY.step(-stepsPerRevolution);
+  myStepperZ.step(-stepsPerRevolution);
+  myStepperE0.step(-stepsPerRevolution);
+  myStepperE1.step(-stepsPerRevolution);
+  myStepperO.step(-stepsPerRevolution);
   digitalWrite(LED_BUILTIN, HIGH);
+  
   delay(2000);
 }
