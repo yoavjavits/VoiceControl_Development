8bc04cd3a2487bf3da6f4fb13da995cd4d5704f5
Gilad Shmerler <shmerler@campus.technion.ac.il>
2022-05-25 22:00:57 +0300

    changes in folders and connection

---
+++ Software/esp_to_arduino_connection/esp_make_connection/esp_make_connection.ino
@@ -0,0 +1,23 @@
+//
+//  ESP32 code
+//
+
+
+#define RXp2 16
+#define TXp2 17
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial2.begin(115200, SERIAL_8N1, RXp2, TXp2);
+}
+void loop() {
+  for (int i =0; i<301; i++)
+  {
+    Serial2.println(i);
+    //Serial.println(Serial2.readString());
+    Serial.print("sent packet ");Serial.println(i);
+    delay(500);
+    
+  }
+}
