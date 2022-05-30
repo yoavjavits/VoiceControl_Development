94d401a96a9d5947ca76eff871973f39cf565da2
morlevy <84244797+morlevy@users.noreply.github.com>
2022-05-30 13:31:31 +0300

    Add files via upload

---
+++ Application/voicecontrol/GLWorld.java
@@ -0,0 +1,82 @@
+package com.example.voicecontrol;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class GLWorld {
+
+    public void addShape(GLShape shape) {
+        mShapeList.add(shape);
+        mIndexCount += shape.getIndexCount();
+    }
+
+    public void generate() {
+        ByteBuffer bb = ByteBuffer.allocateDirect(mVertexList.size()*4*4);
+        bb.order(ByteOrder.nativeOrder());
+        mColorBuffer = bb.asIntBuffer();
+
+        bb = ByteBuffer.allocateDirect(mVertexList.size()*4*3);
+        bb.order(ByteOrder.nativeOrder());
+        mVertexBuffer = bb.asIntBuffer();
+
+        bb = ByteBuffer.allocateDirect(mIndexCount*2);
+        bb.order(ByteOrder.nativeOrder());
+        mIndexBuffer = bb.asShortBuffer();
+
+        Iterator iter2 = mVertexList.iterator();
+        while (iter2.hasNext()) {
+            GLVertex vertex = (GLVertex)iter2.next();
+            vertex.put(mVertexBuffer, mColorBuffer);
+        }
+
+        Iterator iter3 = mShapeList.iterator();
+        while (iter3.hasNext()) {
+            GLShape shape = (GLShape) iter3.next();
+            shape.putIndices(mIndexBuffer);
+        }
+    }
+
+    public GLVertex addVertex(float x, float y, float z) {
+        GLVertex vertex = new GLVertex(x, y, z, mVertexList.size());
+        mVertexList.add(vertex);
+        return vertex;
+    }
+
+    public void transformVertex(GLVertex vertex, M4 transform) {
+        vertex.update(mVertexBuffer, transform);
+    }
+
+    int count = 0;
+    public void draw(GL10 gl)
+    {
+        mColorBuffer.position(0);
+        mVertexBuffer.position(0);
+        mIndexBuffer.position(0);
+
+        gl.glFrontFace(GL10.GL_CW);
+        gl.glShadeModel(GL10.GL_FLAT);
+        gl.glVertexPointer(3, GL10.GL_FIXED, 0, mVertexBuffer);
+        gl.glColorPointer(4, GL10.GL_FIXED, 0, mColorBuffer);
+        gl.glDrawElements(GL10.GL_TRIANGLES, mIndexCount, GL10.GL_UNSIGNED_SHORT, mIndexBuffer);
+        count++;
+    }
+
+    static public float toFloat(int x) {
+        return x/65536.0f;
+    }
+
+    private ArrayList mShapeList = new ArrayList();
+    private ArrayList mVertexList = new ArrayList();
+
+    private int mIndexCount = 0;
+
+    private IntBuffer mVertexBuffer;
+    private IntBuffer mColorBuffer;
+    private ShortBuffer mIndexBuffer;
+}
